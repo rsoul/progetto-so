2344d0cc992f798d70e6ae9e37932e8ff3a2d567
Mattens15 <matteo.attenni@outlook.it>
2018-02-07 20:04:53 +0100

    rimosso websocket. Ci penso lo script a fare tutto il necessario

--- WebSocket/include/errors.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef __ERRORS_HPP__
-#define __ERRORS_HPP__
-
-#define         ERR_NONE                        0
-#define         ERR_SYSTEM                      1
-#define         ERR_NETWORK                     2
-
-#endif
