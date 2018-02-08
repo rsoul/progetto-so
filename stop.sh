0c7f7c7e304f0f7ba8480cbf6409b817454a3484
djzapo <zapo93@hotmail.it>
2018-02-08 12:15:32 +0000

    script stop.sh perfettamente funzionante

--- stop.sh
+++ stop.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-pid_server=pidof./server 8080
 
-kill $pid_server
+
+pkill -f "./server 8080" 
+
+
+pkill -f "./start.sh"
+
+pkill -f "raspistill -tl 500 -t 999999 -vf -w 960 "
 
 echo "Server killed"
