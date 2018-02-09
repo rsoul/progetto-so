e6a0cf57a19f5cec4ff8ff3d2f6383d7984a8851
Mattens15 <matteo.attenni@outlook.it>
2018-02-09 17:29:18 +0100

    fix

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 echo "Server aperto:localhost:8080 oppure ip:8080"
 while :
 do
-raspistill -tl 6000 -t 999999 -vf -w 960 -h 720 -o stream/test.jpg  -n -q 50
+raspistill -tl 800 -t 999999 -vf -w 960 -h 720 -o stream/test.jpg  -n -q 50
 sleep 1
 done
